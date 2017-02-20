2b9f836b605cf35f8938f5285b4bc82ceb52c49b
brian3t <ngxtri@gmail.com>
2017-02-19 23:31:24 -0800

    Login page part 1 / 4

--- js/constants.js
+++ js/constants.js
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var IS_DEBUG = true;
+// var IS_DEBUG = true;
+var IS_DEBUG = false;
 var USE_MWCOG3 = true;
 // var USE_MWCOG3 = false;
 
