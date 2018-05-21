39bc7721a7a9c5cde6f99b7cce1cd31d023784ad
Tri <ngxtri@gmail.com>
2018-05-21 12:33:09 -0700

    Update rideshare.js
    Fix weird issue on IOS not clearning     localStorage.removeItem('latlng');

--- js/rideshare.js
+++ js/rideshare.js
@@ -112,6 +112,7 @@ function rs_back() {
     } else {
         window.location.href='search.html';
     }
+    window.localStorage.removeItem('latlng');
 }
 
 function populate_ridematch_deeplink(res) {
