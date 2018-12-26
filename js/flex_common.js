ab0324c15dff153a564874d28087a46d8c6dfa5d
brian3t <ngxtri@gmail.com>
2018-12-26 14:41:26 -0800

    Flextime popup now shown. Next: resize them to small width.

--- js/flex_common.js
+++ js/flex_common.js
@@ -1,8 +1,19 @@
-$(document).on("pagecreate", function () {
+$(document).on("pagebeforecreate", function () {
     console.log(`page init done. loading template into #template`);
-    $('#template').load('flex_common.html');
+    // $('#template').load('flex_common.html');
+});
+
+$(document).on('pageshow', ()=>{
+    console.log(`page is shown`);
+
+});
+
+$(document).on('pagechange', ()=> {
+    console.log(`page is changed`);
     let user = ls('user');
     if (user.is_flex){
-
+        setTimeout(()=>$('#flex_incentive_joined_popup').popup('open'), 2000);
+    } else {
+        setTimeout(()=>$('#flex_incentive_invite_popup').popup('open'), 2000);
     }
 });
