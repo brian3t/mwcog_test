89977c3523c57ae6ab48a4c4a82b84a45ae01f2a
Brian Nguyen <ngxtri@gmail.com>
2018-12-26 18:41:09 -0800

    Flextime popup refactor

--- js/flex_common.js
+++ js/flex_common.js
@@ -1,15 +1,14 @@
 $(document).on("pagebeforecreate", function () {
-    console.log(`page init done. loading template into #template`);
-    // $('#template').load('flex_common.html');
+    console.log(`1 page init done. loading template into #template`);
+    $('#template').load('flex_common.html');
 });
 
 $(document).on('pageshow', ()=>{
-    console.log(`page is shown`);
-
+    console.log(`2 page is shown`);
 });
 
 $(document).on('pagechange', ()=> {
-    console.log(`page is changed`);
+    console.log(`3 page is changed`);
     let user = ls('user');
     if (user.is_flex){
         setTimeout(()=>$('#flex_incentive_joined_popup').popup('open'), 2000);
