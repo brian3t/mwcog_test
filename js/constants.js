e788ed6cebee24e00a566bc4de57de545073e5ee
brian3t <ngxtri@gmail.com>
2019-01-20 20:30:58 -0800

    Disable debug mode.

--- js/constants.js
+++ js/constants.js
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-// var IS_DEBUG = false;
-var IS_DEBUG = true;
+var IS_DEBUG = false;
+// var IS_DEBUG = true;
 var IS_SIMULATE_DEEPLINK = false;
 // var IS_SIMULATE_DEEPLINK = true;
 var USE_MWCOG = false;
