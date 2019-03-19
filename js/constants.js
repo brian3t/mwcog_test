6ba3c5198d4fd24f9cd9b0e3fedc1a2342b1cb29
brian3t <ngxtri@gmail.com>
2019-03-18 22:22:53 -0700

    • Prevent user from starting a trip if your selected destination is too close. • Add warning if user is manually cancelling a trip

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 var IS_DEBUG = false;
 // var IS_DEBUG = true;
 var IS_SIMULATE_DEEPLINK = false;
